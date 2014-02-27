b49b84a834ed3fc0b269cbfb05fab3ec1dfbcf0d
Elaine Zhou <ezhou@stanford.edu>
2014-02-27 19:49:13 +0000

    add stretch goal

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,7 +9,16 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$('.likeBtn').click(function(){
+	var startTime = new Date().getTime();
+	$('.likeBtn').click(likeCallback);
+
+	function likeCallback(e){
+
+		var endTime = new Date().getTime();
+		var timeSpent = endTime - startTime;
+
 		ga("send", "event", "like", "click");
-	})
+		ga('send', 'timing', 'TimeTillLike', timeSpent);
+	}
 }
+
