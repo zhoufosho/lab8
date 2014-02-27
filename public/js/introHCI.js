4411ad8d1e309ddc83f330feed174d6cdc854361
Elaine Zhou <ezhou@stanford.edu>
2014-02-27 19:10:14 +0000

    initial analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,7 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$('.likeBtn').click(function(){
+		ga("send", "event", "like", "click");
+	})
 }
